2e0588e5cca49a749a8baf81baa98ff37dad33c6
jstancek <jan.stancek@ceeor.com>
2015-12-08 20:39:22 +0100

    changes

---
+++ moment-examples.js
@@ -0,0 +1,20 @@
+var moment = require('moment');
+
+var now = moment();
+
+//console.log(now.format());
+//
+//console.log(now.format('X'));
+//
+//console.log(now.valueOf());
+
+var timestamp = 1449602164683;
+var timestampMoment = moment.utc(timestamp);
+
+console.log(timestampMoment.local().format('h:mm a'));
+
+
+//now.subtract(1, 'year');
+//console.log(now.format());
+//
+//console.log(now.format('Do-MMM-YYYY, h:mm:ss a'));
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http); // calling directly
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,12 +13,18 @@ io.on('connection', function (socket) {
     socket.on('message', function (message){
         console.log('Message received ' + message.text);
 
+        message.timeStamp = moment().valueOf();
+
         //socket.broadcast.emit('message', message); this will lead to view messages of other users
         io.emit('message', message); // this leads to view messages of the user who wrote it
     });
 
+    // timestamp property - Javascript timestamp (milliseconds)
+
+
     socket.emit('message', {
-        text: 'Welcome to chat application'
+        text: 'Welcome to chat application',
+        timestamp: moment().valueOf()
     });
 });
 
