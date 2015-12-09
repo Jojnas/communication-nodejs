82110d040ccb8e488fc7fffe8fa76c3f8c42af3d
jstancek <jan.stancek@ceeor.com>
2015-12-09 21:14:52 +0100

    Add features

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 io.on('connection', function (socket) {
     console.log("user connected via socket.io");
+
+    socket.on('joinRoom', function(request){
+        clientInfo[socket.id] = request;
+        socket.join(request.room);
+        socket.broadcast.to(request.room).emit('message', {
+            name: 'System',
+            text: request.name + ' has joined',
+            timestamp: moment().valueOf()
+        });
+    });
 
     socket.on('message', function (message){
         console.log('Message received ' + message.text);
@@ -16,7 +28,7 @@ io.on('connection', function (socket) {
         message.timeStamp = moment().valueOf();
 
         //socket.broadcast.emit('message', message); this will lead to view messages of other users
-        io.emit('message', message); // this leads to view messages of the user who wrote it
+        io.to(clientInfo[socket.id].room).emit('message', message); // this leads to view messages of the user who wrote it
     });
 
     // timestamp property - Javascript timestamp (milliseconds)
